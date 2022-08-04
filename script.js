2a30d151926788eb489a5fec0c7f22938441a992
ThiagoMachadoL <thiagojkp123@gmail.com>
2022-08-03 21:13:14 -0300

    Aula - 12

---
+++ script.js
@@ -0,0 +1,55 @@
+// const bool1 = false;
+// const bool2 = true; 
+// const bool3 = true;
+
+// // if (bool1) {
+// //     alert('Entrei no if, sou true');   
+// // } else {
+// //     alert('Entrei no else, sou false');
+// // }
+
+// //exemplo 2
+
+// if (bool1 === bool2) {
+//     alert('1 e 2 são iguais');   
+// } else if(bool2 === bool3) {
+//     alert('2 e 3 são iguais');
+// } else if(bool1 === bool3) {
+//     alert('1 e 3 são iguais');
+// } else {
+//     alert('Nenhuma das variáveis são iguais')
+// }
+
+// const idade = 26;
+
+// //modo 1
+// if (idade >= 60){
+//     alert("o voto é facultativo");
+// } else if (idade >= 16 && idade < 18) {
+//     alert("Voto é facultativo ");
+// } else if(idade >= 18) {
+//     alert("Deve emitir o título");
+// } else {
+//     alert("Não pode emitir o título");
+// };
+// //modo 2
+
+// if (idade >= 60 || idade >= 16 && idade < 18) {
+//     alert("o voto é facultativo");
+// } else if(idade >= 18) {
+//     alert("Deve emitir o título");
+// } else {
+//     alert("Não pode emitir o título");
+// };
+
+const media = 10;
+if(media >=5){
+    alert("Aprovado");
+}else if(media <=3){
+    alert("Recuperação");
+}else if(media <3) {
+    alert("Reprovado");
+}else{
+    alert("Dado inválido");
+}
+
